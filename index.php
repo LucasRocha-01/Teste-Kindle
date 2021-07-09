df83aee9db8abdc4af8e7c4743b5fccc8e01d37b
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2021-07-09 11:15:48 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                             <img class="img-post-unico" src=<?php the_post_thumbnail(); ?>
                             <h2><?php the_title(); ?></h2>
                             <p><?php the_content(); ?></p>
-                            <a href=<?php the_permalink(); ?>>Leia Mais</a>
+                            <a href=<?php the_permalink(); ?> class="link-post-unico" >Leia Mais</a>
                         </div>
                     <?php endwhile;?>
 
